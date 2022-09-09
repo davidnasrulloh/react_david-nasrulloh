0d23e03fe07b6463efd3a6f3758f510d11d0ad4f
David Nasrulloh <davidkrb52@gmail.com>
2022-09-09 21:13:00 +0700

    feat: menambahkan file dave.js challenge kelas

---
+++ 10_Introduction Algorithm/praktikum/dave.js
@@ -0,0 +1,18 @@
+const findMyPet = (name) => {
+    let hewan;
+    let indexH;
+
+    const pets = myPets.map((pet) => {
+        if (name == pet) {
+            hewan = name;
+            indexH = myPets.indexOf(pet);
+        }
+    });
+    console.log(hewan + " ada diruangan " + indexH);
+};
+
+const myPets = ["Ayam", "Ikan", "Iguana", "Kecoak"];
+
+findMyPet("Ikan");
+
+// Ikan kamu ada diruangan 1
